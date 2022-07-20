a5070b7b643b9fbb0c037d535c47a2eb0adb2cfe
Vitwon <vitordeaguiar0@gmail.com>
2022-07-20 15:05:49 -0300

    #02 - Commit Length

--- cypress/e2e/testSeach.cy.js
+++ cypress/e2e/testSeach.cy.js
@@ -4,10 +4,7 @@ describe('Busca teste bitcoin', ()=>{
     it.only('teste de busca com enter',()=>{
         cy.visit('https://duckduckgo.com');
         cy.get('#search_form_input_homepage').type('Bitcoin{enter}');
-        cy.get('#links_wrapper').then(elements => {
-            cy.log('Number of search results found', elements.length);
-          })     
-
+        cy.get('.nrn-react-div').should('have.length',10);
     })
 
 
@@ -15,7 +12,7 @@ describe('Busca teste bitcoin', ()=>{
         cy.visit('https://duckduckgo.com');
         cy.get('#search_form_homepage').type('Bitcoin');
         cy.get('#search_button_homepage').click();
-        cy.intercept('GET', 'https://duckduckgo.com/**').as('getDuck');
+        cy.get('.nrn-react-div').should('have.length', 10);
     })
     
 })
